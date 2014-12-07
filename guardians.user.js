b393ea1bfbf5c03291847017083d8fa853ebe38f
Kevin Nowaczyk <kevin@semanticpillow.com>
2014-12-07 17:17:19 +0000

    Comment out build environment lines

--- guardians.user.js
+++ guardians.user.js
@@ -21,9 +21,9 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 
 //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
 //(leaving them in place might break the 'About IITC' page or break update checks)
-plugin_info.buildName = 'jonatkins-test';
-plugin_info.dateTimeVersion = '20141206.72845';
-plugin_info.pluginId = 'guardians';
+//plugin_info.buildName = 'jonatkins-test';
+//plugin_info.dateTimeVersion = '20141206.72845';
+//plugin_info.pluginId = 'guardians';
 //END PLUGIN AUTHORS NOTE
 
 
